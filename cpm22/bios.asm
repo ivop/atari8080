5eb3f64c5a2743b0779d61addea394c12b8e7dc7
Ivo van Poorten <ivopvp@gmail.com>
2023-05-27 10:20:57 +0200

    Calculate all parameters, similar to CP/M 65

--- cpm22/bios.asm
+++ cpm22/bios.asm
@@ -75,22 +75,81 @@ trans:
 
 ; disk parameter block
 
+number_of_tracks = 77
+reserved_tracks = 2
+sectors_per_track = 26
+block_size = 1024
+dirents = 64
+
+    if block_size = 1024
+block_shift = 3
+    elseif block_size = 2048
+block_shift = 4
+    elseif block_size = 4096
+block_shift = 5
+    elseif block_size = 8192
+block_shift = 6
+    elseif block_size = 16384
+block_shift = 7
+    endif
+
+block_mask = (1 << block_shift) - 1
+
+checksum_buffer_size = (dirents + 3) / 4
+sectors = (number_of_tracks - reserved_tracks) * sectors_per_track
+blocks_on_disk = sectors * 128 / block_size
+allocation_vector_size = (blocks_on_disk + 7) / 8
+directory_blocks = (dirents * 32) / block_size
+allocation_bitmap = (0ffffh << (16 - directory_blocks)) & 0ffffh
+
+    if directory_blocks = 0
+        error "Directory must be at least one block in size!"
+    endif
+    if (dirents * 32) # block_size != 0
+        error "Directory is not an even number of blocks in size!"
+    endif
+
+    if blocks_on_disk < 256
+        if block_size = 1024
+extent_mask = 000h              ; %00000000
+        elseif block_size = 2048
+extent_mask = 001h              ; %00000001
+        elseif block_size = 4096
+extent_mask = 003h              ; %00000011
+        elseif block_size = 8192
+extent_mask = 007h              ; %00000111
+        elseif block_size = 16384
+extent_mask = 00fh              ; %00001111
+        endif
+    else
+        if block_size = 1024
+            error "Cannot use a block size of 1024 on a large disk!"
+        elseif block_size = 2048
+extent_mask = 000h              ; %00000000
+        elseif block_size = 4096
+extent_mask = 001h              ; %00000001
+        elseif block_size = 8192
+extent_mask = 003h              ; %00000011
+        elseif block_size = 16384
+extent_mask = 007h              ; %00000111
+        endif
+    endif
+
 dpblk:
-    dw 26       ; sectors per track
-    db 3        ; block shift factor
-    db 7        ; block mask
-    db 0        ; null mask (extent mask)
-    dw 243-1    ; disk size in blocks - 1, excluding reserved tracks
-    dw 64-1     ; directory entries - 1
-    db 192      ; alloc hi
-    db 0        ; alloc lo
-    dw 16       ; check size 
-    dw 2        ; track offset (reserved tracks)
+    dw sectors_per_track
+    db block_shift
+    db block_mask
+    db extent_mask
+    dw blocks_on_disk - 1
+    dw dirents - 1
+    db (allocation_bitmap & 0ff00h)>>8
+    db (allocation_bitmap & 000ffh)
+    dw checksum_buffer_size
+    dw reserved_tracks
 
 dirbf:
     ds 128      ; can be the same for multiple disks
 chk00:
-    ds 16       ; check vector
+    ds checksum_buffer_size
 all00:
-    ds 31       ; allocation vector size bytes
-
+    ds allocation_vector_size
